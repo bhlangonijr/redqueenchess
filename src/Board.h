fd31188b373cb3993cb2c3d44d7ee224ff76ad21
bhlangonijr <bhlangonijr@865e88f3-cc1b-4e5f-86d1-2a7dcd23a5c5>
2009-04-05 02:45:56 +0000

    Created diagonal bitboard constants
    git-svn-id: https://svn.code.sf.net/p/redqueenchess/code/redqueen@8 865e88f3-cc1b-4e5f-86d1-2a7dcd23a5c5

--- src/Board.h
+++ src/Board.h
@@ -34,17 +34,18 @@ namespace BoardTypes{
 //Bitboard type - unsigned long int (8 bytes)
 typedef uint64_t Bitboard;
 
-#define USE_INTRINSIC_BITSCAN													// will use hardware's bitscan
-#define ALL_PIECE_TYPE 			6   											// pawn, knight, bishop, rook, queen, king
-#define ALL_PIECE_TYPE_BY_COLOR 13  											// (black, white) X (pawn, knight, bishop, rook, queen, king) + empty
-#define ALL_PIECE_COLOR			3   											// black, white, none
-#define ALL_SQUARE				64  											// all square A1 .. H8
-#define ALL_RANK				8												// all ranks
-#define ALL_FILE				8												// all files
+#define USE_INTRINSIC_BITSCAN																		// will use hardware's bitscan
+#define ALL_PIECE_TYPE 			6   																// pawn, knight, bishop, rook, queen, king
+#define ALL_PIECE_TYPE_BY_COLOR 13  																// (black, white) X (pawn, knight, bishop, rook, queen, king) + empty
+#define ALL_PIECE_COLOR			3   																// black, white, none
+#define ALL_SQUARE				64  																// all square A1 .. H8
+#define ALL_RANK				8																	// all ranks
+#define ALL_FILE				8																	// all files
+#define ALL_DIAGONAL			15																	// all diagonals
 
-#define Bb2Sq2(X)				log2(X)											// Bitboard to square enum version 1
-#define Sq2Bb(X)				squareToBitboard[X] 							// square to bitboard macro
-#define St2Sq(F,R)				(((int)F-96)+((int)R-49)*8)-1					// encode String to Square enum
+#define Bb2Sq2(X)				log2(X)																// Bitboard to square enum version 1
+#define Sq2Bb(X)				squareToBitboard[X] 												// square to bitboard macro
+#define St2Sq(F,R)				(((int)F-96)+((int)R-49)*8)-1										// encode String to Square enum
 #define Sq2RA(X)				(fileBB[squareFile[X]]|rankBB[squareRank[X]])^squareToBitboard[X]  	// Encode Square to Rook Attack
 
 #define INITIAL_WHITE_BITBOARD  	 0xFFFFULL
@@ -96,28 +97,42 @@ enum Square {
 	A8, B8, C8, D8, E8, F8, G8, H8,
 	NONE
 };
+
 //colors
 enum PieceColor {
 	WHITE, BLACK, COLOR_NONE
 };
+
 //piece types
 enum PieceType {
 	PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
 };
+
 // piece type by color
 enum PieceTypeByColor {
 	WHITE_PAWN, WHITE_KNIGHT, WHITE_BISHOP, WHITE_ROOK, WHITE_QUEEN, WHITE_KING, BLACK_PAWN, BLACK_KNIGHT, BLACK_BISHOP, BLACK_ROOK, BLACK_QUEEN, BLACK_KING, EMPTY
 };
+
 // castle types
 enum CastleRight {
 	NO_CASTLE, KING_SIDE_CASTLE, QUEEN_SIDE_CASTLE, BOTH_SIDE_CASTLE
 };
+
 //color of a given piece
 static const PieceColor pieceColor[ALL_PIECE_TYPE_BY_COLOR] = {WHITE,WHITE,WHITE,WHITE,WHITE,WHITE,BLACK,BLACK,BLACK,BLACK,BLACK,BLACK,COLOR_NONE};
+
 //ranks - row
 enum Rank { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8 };
+
 //files - column
 enum File { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H };
+
+// diagonals A1..H8
+enum DiagonalA1H8 { A8_A8, B8_A7, C8_A6, D8_A5, E8_A4, F8_A3, G8_A2, H8_A1, B1_H7, C1_H6, D1_H5, F1_H4, F1_H3, G1_H2, H1_H1 };
+
+// diagonals A1..H8
+enum DiagonalH1A8 { A1_A1, B1_A2, C1_A3,D1_A4, E1_A5, F1_A6, G1_A7, H1_A8, B8_H2, C8_H3, D8_H4, F8_H5, F8_H6, G8_H7, H8_H8 };
+
 // Rank of a given square
 static const Rank squareRank[ALL_SQUARE]={
 		RANK_1, RANK_1, RANK_1, RANK_1, RANK_1, RANK_1, RANK_1, RANK_1,
@@ -160,6 +175,40 @@ static const Bitboard rankBB[ALL_RANK]={0x00000000000000FFULL,0x000000000000FF00
 // bitboard for all files
 static const Bitboard fileBB[ALL_FILE]={0x0101010101010101ULL,0x0202020202020202ULL,0x0404040404040404ULL,0x0808080808080808ULL,
 										0x1010101010101010ULL,0x2020202020202020ULL,0x4040404040404040ULL,0x8080808080808080ULL};
+
+// bitboard for all diagonal A1..H8
+static const Bitboard diagonalA1H8BB[ALL_DIAGONAL]={Sq2Bb(A8),
+													Sq2Bb(B8)|Sq2Bb(A7),
+													Sq2Bb(C8)|Sq2Bb(B7)|Sq2Bb(A6),
+													Sq2Bb(D8)|Sq2Bb(C7)|Sq2Bb(B6)|Sq2Bb(A5),
+													Sq2Bb(E8)|Sq2Bb(D7)|Sq2Bb(C6)|Sq2Bb(B5)|Sq2Bb(A4),
+													Sq2Bb(F8)|Sq2Bb(E7)|Sq2Bb(D6)|Sq2Bb(C5)|Sq2Bb(B4)|Sq2Bb(A3),
+													Sq2Bb(G8)|Sq2Bb(F7)|Sq2Bb(E6)|Sq2Bb(D5)|Sq2Bb(C4)|Sq2Bb(B3)|Sq2Bb(A2),
+													Sq2Bb(H8)|Sq2Bb(G7)|Sq2Bb(F6)|Sq2Bb(E5)|Sq2Bb(D4)|Sq2Bb(C3)|Sq2Bb(B2)|Sq2Bb(A1),
+													Sq2Bb(B1)|Sq2Bb(C2)|Sq2Bb(D3)|Sq2Bb(E4)|Sq2Bb(F5)|Sq2Bb(G6)|Sq2Bb(H7),
+													Sq2Bb(C1)|Sq2Bb(D2)|Sq2Bb(E3)|Sq2Bb(F4)|Sq2Bb(G5)|Sq2Bb(H6),
+													Sq2Bb(D1)|Sq2Bb(E2)|Sq2Bb(F3)|Sq2Bb(G4)|Sq2Bb(H5),
+													Sq2Bb(F1)|Sq2Bb(F2)|Sq2Bb(G3)|Sq2Bb(H4),
+													Sq2Bb(F1)|Sq2Bb(G2)|Sq2Bb(H3),
+													Sq2Bb(G1)|Sq2Bb(H2),
+													Sq2Bb(H1) };
+
+// bitboard for all diagonal H1..A8
+static const Bitboard diagonalH1A8BB[ALL_DIAGONAL]={Sq2Bb(A1),
+													Sq2Bb(B1)|Sq2Bb(A2),
+													Sq2Bb(C1)|Sq2Bb(B2)|Sq2Bb(A3),
+													Sq2Bb(D1)|Sq2Bb(C2)|Sq2Bb(B3)|Sq2Bb(A4),
+													Sq2Bb(E1)|Sq2Bb(D2)|Sq2Bb(C3)|Sq2Bb(B4)|Sq2Bb(A5),
+													Sq2Bb(F1)|Sq2Bb(E2)|Sq2Bb(D3)|Sq2Bb(C4)|Sq2Bb(B5)|Sq2Bb(A6),
+													Sq2Bb(G1)|Sq2Bb(F2)|Sq2Bb(E3)|Sq2Bb(D4)|Sq2Bb(C5)|Sq2Bb(B6)|Sq2Bb(A7),
+													Sq2Bb(H1)|Sq2Bb(G2)|Sq2Bb(F3)|Sq2Bb(E4)|Sq2Bb(D5)|Sq2Bb(C6)|Sq2Bb(B7)|Sq2Bb(A8),
+													Sq2Bb(B8)|Sq2Bb(C7)|Sq2Bb(D6)|Sq2Bb(E5)|Sq2Bb(F4)|Sq2Bb(G3)|Sq2Bb(H2),
+													Sq2Bb(C8)|Sq2Bb(D7)|Sq2Bb(E6)|Sq2Bb(F5)|Sq2Bb(G4)|Sq2Bb(H3),
+													Sq2Bb(D8)|Sq2Bb(E7)|Sq2Bb(F6)|Sq2Bb(G5)|Sq2Bb(H4),
+													Sq2Bb(F8)|Sq2Bb(F7)|Sq2Bb(G6)|Sq2Bb(H5),
+													Sq2Bb(F8)|Sq2Bb(G7)|Sq2Bb(H6),
+													Sq2Bb(G8)|Sq2Bb(H7),
+													Sq2Bb(H8) };
 
 // bitboard for all rook attacks
 static const Bitboard rookAttacks[ALL_SQUARE]={	Sq2RA(A1), Sq2RA(B1), Sq2RA(C1), Sq2RA(D1), Sq2RA(E1), Sq2RA(F1), Sq2RA(G1), Sq2RA(H1),
